a7d42f80519dc01ed956106509676653686c5b9a
ziscloud <ziscloud@gmail.com>
2014-09-25 18:07:32 +0800

    Subject: update package.json file

--- example/main.js
+++ example/main.js
@@ -1,6 +1,6 @@
 angular
     .module('angular-footable-example', [
-        'angular-footable.example'
+        'angular-footable'
     ])
     .controller('exampleCtrl', function($scope, example) {
         $scope.exampleResult = example();
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
     boilerplate = require('boilerplate-gulp');
+    
 boilerplate(gulp, {
     pkg: require('./package.json'),
     jsMain: './src/angular-footable.js'
