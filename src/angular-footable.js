e51b4727649b16725cd990131cf0bd5d6bd6664f
alex <alexandra.izaguirre@macate.com>
2016-05-06 15:49:15 -0700

    Added function() wrapper for safety in concatenation, removed unnecessary redrawing of the footable and added checks for loadWhen attribute presence and watch for data loading (currently supports array object), removed unnecessary timeout

--- src/angular-footable.js
+++ src/angular-footable.js
@@ -1,65 +1,75 @@
-'use strict';
-
-angular
-    .module('ui.footable', [])
-    .directive('footable', ['$timeout', function($timeout) {
-        var events = {
-            beforeFiltering: 'footable_filtering'
-        };
-        var extractSpecOpts = function(opts, attrs) {
-            var extracted = {},
-                k;
-            for (k in opts) {
-                if (k !== 'filter' && (!angular.isUndefined(events[k]))) {
-                    if(!angular.isFunction(scope.$eval(attrs[k]))) {
-                        extracted[k] = attrs[k];
+/**
+ * License: MIT
+ */
+(function(angular) {
+    'use strict';
+    angular
+        .module('ui.footable', [])
+        .directive('footable', function() {
+            var events = {
+                beforeFiltering: 'footable_filtering'
+            };
+            var extractSpecOpts = function(opts, attrs) {
+                var extracted = {},
+                    k;
+                for (k in opts) {
+                    if (k !== 'filter' && (!angular.isUndefined(events[k]))) {
+                        if(!angular.isFunction(scope.$eval(attrs[k]))) {
+                            extracted[k] = attrs[k];
+                        }
                     }
                 }
-            }
-            return extracted;
-        };
+                return extracted;
+            };
 
-        var bindEventHandler = function(tableObj, scope, attrs) {
-            var k;
-            for (k in attrs) {
-                if (k !== 'filter' && (!angular.isUndefined(events[k]))) {
-                    var targetEventName = events[k];
-                    if(angular.isFunction(scope.$eval(attrs[k]))) {
-                        tableObj.bind(targetEventName, scope.$eval(attrs[k]));
+            var bindEventHandler = function(tableObj, scope, attrs) {
+                var k;
+                for (k in attrs) {
+                    if (k !== 'filter' && (!angular.isUndefined(events[k]))) {
+                        var targetEventName = events[k];
+                        if(angular.isFunction(scope.$eval(attrs[k]))) {
+                            tableObj.bind(targetEventName, scope.$eval(attrs[k]));
+                        }
                     }
                 }
-            }
-        };
+            };
 
-        return {
-            restrict: 'C',
-            link: function(scope, element, attrs) {
-                var tableOpts = {
-                    'event-filtering': null
-                };
-                $timeout(function(){
+            return {
+                restrict: 'C',
+                scope: {
+                    loadWhen: '='
+                },
+                link: function(scope, element, attrs) {
+                    var tableOpts = {
+                        'event-filtering': null
+                    };
                     angular.extend(
                         tableOpts,
                         footable.options
                     );
-
                     angular.extend(
                         tableOpts,
                         extractSpecOpts(tableOpts, attrs)
                     );
                     var tableObj = {};
-                    if(typeof element.footable === 'function'){
-                        tableObj = element.footable(tableOpts); 
-                    } else {
-                        tableObj = jQuery(element).footable(tableOpts);
-                    }
-                    bindEventHandler(tableObj, scope, attrs);
-                    scope.$watch(function() {return attrs.loadWhen; }, function(){
-                        $timeout(function(){
-                            element.triggerHandler('footable_redraw');
+                    var initTable = function(){
+                        if(typeof element.footable === 'function'){
+                            tableObj = element.footable(tableOpts); 
+                        } else {
+                            tableObj = jQuery(element).footable(tableOpts);
+                        }
+                        bindEventHandler(tableObj, scope, attrs);
+                    };
+                    if(typeof attrs.loadWhen !== 'undefined'){
+                        scope.$watch(function(){return scope.loadWhen; }, function(data){
+                            if(typeof data!== 'undefined'&&data.length){
+                                initTable();
+                            }
                         });
-                    });
-                },1000);
-            }
-        };
-    }]);
+                    } else {
+                        initTable();
+                    }
+                }
+            };
+        });
+})(angular);
